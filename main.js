717f36a47e7ce5d44a2818f330805b866bd9db37
gisse <gisselyespinola@gmail.com>
2025-01-15 15:47:29 -0300

    cambios

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 
-/*
-Usuario
-Tweet
-Comentario
-Likes
-
-*/
-
-app.get('/saludar', function(req, res) {
-    res.send('Hola Mundo')
+app.get('/tweet', async function(req, res) {
+   const tweets = await prisma.tweet.findMany()
+   res.json(tweets)
 })
 
 app.listen(3000)
