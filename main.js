895cbad58167668af3f901732a6c4253c3d5a888
Kevin Vazquez <kv937398@gmail.com>
2025-01-15 15:45:50 -0300

    Cambios

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 
-/*
-
-Usuarios
-Tweets
-Comentarios
-Likes
-
-*/
-
-app.get('/saludar', function (req, res){
-    res.send('Hola mundo')
+app.get('/tweet', async function (req, res){
+    const tweets = await prisma.tweet.findMany()
+    res.json(tweets)
 })
 
 app.listen(3000)
