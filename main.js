5b1f2df7c29b726d6eae4563d7df2d82352e3f86
julio <aacesar18@gmail.com>
2025-01-15 15:50:51 -0300

    cambios

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma=new PrismaClient()
 const app = express()
 
 /*
 Usuarios , tweets ,conversacion , likes
 */
+/*
 app.get('/saludar',function(req,res){
     res.send('Hola mundo')
 })
+*/
+app.get('/tweet', async function(req,res){
+    const tweets=await prisma.tweet.findMany()
+    res.json(tweets)
+})
 app.listen(3000)
+
