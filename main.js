b3e1ab0c8dceade0bb2eb9cf1a2e8c717cf18d45
Elias Cabrera <josecabrera.jeacbt@gmail.com>
2025-01-15 15:50:22 -0300

    Cambio1

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import express from "express"
+import {PrismaClient} from "@prisma/client"
 
+const prisma = new PrismaClient()
 const app = express()
 
-app.get("/saludar", function(req, res){
-    
+app.get("/tweet", async function(req, res){
+    const tweets = await prisma.tweet.findMany()
+    res.json(tweets) 
+ 
 })
+
 app.listen(3000)
