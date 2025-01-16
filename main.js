e66675debb095005340354ff793101341dcef094
junior <chamqui0gmail.com>
2025-01-16 17:12:28 -0300

    3clase

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-import express from "express"
 
-const app = express()
-import { PrismaClient } from "@prisma/client"
+
+import express from 'express'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 /*
 susuarios
 tweets
@@ -19,13 +22,95 @@ app.post("/enviar", function (req,res){
     const edad = req.query.edad
     res.send("hola "+nombre+" tu edad es: "+ edad)
 }) */
+const app = express()
 
-const prisma = new PrismaClient()
+app.get('/usuario', async function (req, res){
+    const usuario = await prisma.user.findMany()
+    res.json(usuario)
+})
 
-app.get ("/tweets",async function (req,res){
-    const app = await prisma.tweets.findMany()
+
+app.get('/user', async function (req, res){
+  
+    
+    try {
+        const nombre = req.query.nombre
+        const email = req.query.email
+        const password = req.query.password
+
+        const user = await prisma.user.create({
+            data: {
+                nombre,
+                email, 
+                password
+            }
+        })
+        res.json(user)
+    }catch (error){
+        res.json({message: "El email ya exixte, pon otro"})
+    }
+    
+})
+
+app.get('/tweet', async function (req, res){
+    const contenido = req.query.contenido
+    const userId = req.query.userId
+    const idconvertido = parseInt(userId)
+    const tweet = await prisma.tweet.create({
+        data : {
+            contenido,
+            userId : idconvertido
+        }
+    })
+    res.json(tweet)
+})
+
+app.get('/tweets', async function (req, res){
+    const tweets = await prisma.tweet.findMany()
     res.json(tweets)
 })
-app.listen(3000)
+
+app.get('/tweet/:id/like', async function (req, res){
+    const id = req.params.id
+    const idConvertido = parseInt(id)
+    const tweet = await prisma.tweet.update({
+        where: {
+            id: idConvertido
+        },
+        data: {
+            likes: {
+                increment: 1
+            }
+        }
+    })
+    res.json(tweet)
+})
+
+app.get('/login', async function (req, res){
+    try {
+        const email = req.query.email
+        const password = req.query.password
+
+
+        if(!email && !password){
+            return res.json({message: "Ingrese el email y la contraseña"})
+        }
+        const user = await prisma.user.findFirst({
+            where: {
+                email,
+                password
+            }
+        })
+        if(user){
+            res.json({message: "Has iniciado sesion"})
+        }else {
+            res.json({message: "emeil o password incorrecto"})
+        }
+    }catch (error){
+        res.json({message: "Hubo un error en el servidor"})
+    }
+})
+app.listen(3000)    
+
 
 
