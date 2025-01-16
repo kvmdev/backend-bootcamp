5aa8cc211b153ab0ebd00d1df8fc80830df77119
Kevin Vazquez <kv937398@gmail.com>
2025-01-16 14:43:58 -0300

    Added the endpoint that creates tweets by user id

--- main.js
+++ main.js
@@ -5,9 +5,38 @@ const prisma = new PrismaClient()
 
 const app = express()
 
+app.get('/usuarios', async function (req, res){
+    const usuarios = await prisma.user.findMany()
+    res.json(usuarios)
+})
+
+app.get('/usuario', async function(req, res){
+    const nombre = req.query.nombre
+    const email = req.query.email
+    const password = req.query.password
+
+    const usuario = await prisma.user.create({
+        data: {
+            nombre, 
+            email,
+            password
+        }
+    })
+    res.json(usuario)
+})
+
 app.get('/tweet', async function (req, res){
-    const tweets = await prisma.tweet.findMany()
-    res.json(tweets)
+    const contenido = req.query.contenido
+    const userId = req.query.userId
+    const idConvertido = parseInt(userId)
+
+    const tweet = await prisma.tweet.create({
+        data: {
+            contenido,
+            userId: idConvertido
+        }
+    })
+    res.json(tweet)
 })
 
 app.listen(3000)
