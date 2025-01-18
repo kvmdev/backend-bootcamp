335e72a878c7b44aa33c17c0cf66dbf4c0c9b8c8
Kevin Vazquez <kv937398@gmail.com>
2025-01-18 15:52:02 -0300

    Implemented controllers to the endpoints

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import session from "express-session";
-
-const prisma = new PrismaClient();
+import session from "express-session"
+import { createTweet, getTweets, likeTweet } from "./controllers/TweetController.js";
+import { getUsuarios, login, registrar } from "./controllers/UserController.js";
 
 const app = express();
 
@@ -11,98 +11,20 @@ app.use(
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Cambiar a 'false' para desarrollo local
+    cookie: { secure: false, maxAge: 60000 } // Cambiar a 'false' para desarrollo local
   })
 );
 
-app.get("/usuarios", async function (req, res) {
-  const usuarios = await prisma.user.findMany();
-  res.json(usuarios);
-});
+app.get("/usuarios", getUsuarios);
 
-app.get("/usuario", async function (req, res) {
-  try {
-    const nombre = req.query.nombre;
-    const email = req.query.email;
-    const password = req.query.password;
+app.get("/registrar", registrar);
 
-    const usuario = await prisma.user.create({
-      data: {
-        nombre,
-        email,
-        password,
-      },
-    });
-    res.json(usuario);
-  } catch (error) {
-    res.json({ message: "Email en uso" });
-  }
-});
+app.get("/login", login);
 
-app.get("/login", async function (req, res) {
-  try {
-    const email = req.query.email;
-    const password = req.query.password;
+app.get("/tweet", createTweet);
 
-    if (!email && !password) {
-      return res.json({ message: "Ingrese email y contraseña" });
-    }
+app.get("/tweets", getTweets);
 
-    const user = await prisma.user.findFirst({
-      where: {
-        email,
-        password,
-      },
-    });
-
-    if (user) {
-      req.session.user = user; // Guardar en la sesión antes de enviar la respuesta
-      return res.json({ message: "Has iniciado sesion", user });
-    } else {
-      res.json({ message: "email o password incorrectos" });
-    }
-  } catch (error) {
-    res.json({ message: "Hubo un error en el servidor" });
-  }
-});
-
-app.get("/tweet", async function (req, res) {
-  if (req.session.user) {
-    const contenido = req.query.contenido;
-    const userId = req.session.user.id;
-    const idConvertido = parseInt(userId);
-
-    const tweet = await prisma.tweet.create({
-      data: {
-        contenido,
-        userId: idConvertido,
-      },
-    });
-    return res.json(tweet);
-  } else {
-    res.json({ message: "Primero inicia sesion" });
-  }
-});
-
-app.get("/tweets", async function (req, res) {
-  const tweet = await prisma.tweet.findMany();
-  res.json(tweet);
-});
-
-app.get("/tweet/:id/like", async function (req, res) {
-  const id = req.params.id;
-  const idConvertido = parseInt(id);
-  const tweet = await prisma.tweet.update({
-    where: {
-      id: idConvertido,
-    },
-    data: {
-      likes: {
-        increment: 1,
-      },
-    },
-  });
-  res.json(tweet);
-});
+app.get("/tweet/:id/like", likeTweet);
 
 app.listen(3000);
