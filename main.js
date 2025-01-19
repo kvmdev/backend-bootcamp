6221d70fdedc886f39e2ce42e6b3315d2e428f95
gisse <gisselyespinola@gmail.com>
2025-01-18 23:43:49 -0300

    código organizado

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import session from 'express-session'
+import { createTweet, getTweets, likeTweet } from './Controllers/TweetController.js'
+import { getUsuarios, login, registrar } from './Controllers/UserController.js'
 
 const app = express()
 
-app.get('/tweet', async function(req, res) {
-   const tweets = await prisma.tweet.findMany()
-   res.json(tweets)
-})
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, naxAge: 60000 }
+}))
+
+app.get('/usuarios', getUsuarios)
+
+app.get('/user', registrar)
+
+app.get('/tweet', createTweet)
+
+app.get('/tweets', getTweets)
+
+app.get('/tweet/:id/like', likeTweet)
+
+app.get('/login' , login)
 
 app.listen(3000)
