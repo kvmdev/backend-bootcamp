4c557dd826a07e83f84cd1e7305f7ce1e1e4511d
everdamcde@gmail.com <165707949+EverNunez@users.noreply.github.com>
2025-01-20 14:34:17 -0300

    nuevos cambios

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import express from "express"
-import {PrismaClient} from "@prisma/client"
-const prisma = new PrismaClient()
-const app = express()
-/*
-susuarios
-tweets
-comentarios
-likes
-*/
-/*app.get('/saludar', function (req,res){
-    res.send("hola mundo")
-}) 
-app.post("/enviar", function (req,res){
-    
-})*/
-/*
-app.get ("/saludar", function (req,res){
-    const nombre= req.query.nombre
-    const edad = req.query.edad
-    res.send("hola "+nombre+" tu edad es "+ edad)
-}) 
-    */
-app.get ("/tweets", async function (req,res){
-        const tweets = await prisma.tweet.findMany()
-        res.json(tweets)
-    })
-
-app.listen(3000)
-=======
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import session from "express-session"
@@ -59,4 +28,3 @@ app.get("/tweets", getTweets);
 app.get("/tweet/:id/like", likeTweet);
 
 app.listen(3000);
->>>>>>> kevin
