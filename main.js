c85a474150944ea003f7620ab67a8cf06cb172df
Kevin Vazquez <kv937398@gmail.com>
2025-01-29 16:57:22 -0300

    added cors

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ import { commentTweet, createTweet, getAllTweets, likeTweet } from './controller
 import { login, register } from './controllers/auth.js'
 import dotenv from 'dotenv'
 import { validateToken } from './middleware/auth.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.post('/login', login)
 
